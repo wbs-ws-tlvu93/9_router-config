d6db77a765b1ade32fd322792fcf10f065ff47e3
“tlvu93” <101603072+tlvu93@users.noreply.github.com>
2022-05-25 00:29:52 +0200

    style: :label: Add type for user routes function
    Current version of express does not support typing for the async function in the router yet. Might remove the extra lines in the future...

--- src/repos/user-repo.ts
+++ src/repos/user-repo.ts
@@ -2,106 +2,104 @@ import { IUser } from '@models/user-model';
 import { getRandomInt } from '@shared/functions';
 import orm from './mock-orm';
 
-
-
 /**
  * Get one user.
- * 
- * @param email 
- * @returns 
+ *
+ * @param email
+ * @returns
  */
 async function getOne(email: string): Promise<IUser | null> {
-    const db = await orm.openDb();
-    for (const user of db.users) {
-        if (user.email === email) {
-            return user;
-        }
+  const db = await orm.openDb();
+  const users: IUser[] = db.users;
+  for (const user of users) {
+    if (user.email === email) {
+      return user;
     }
-    return null;
+  }
+  return null;
 }
-
 
 /**
  * See if a user with the given id exists.
- * 
- * @param id 
+ *
+ * @param id
  */
 async function persists(id: number): Promise<boolean> {
-    const db = await orm.openDb();
-    for (const user of db.users) {
-        if (user.id === id) {
-            return true;
-        }
+  const db = await orm.openDb();
+  for (const user of db.users) {
+    if (user.id === id) {
+      return true;
     }
-    return false;
+  }
+  return false;
 }
-
 
 /**
  * Get all users.
- * 
- * @returns 
+ *
+ * @returns
  */
 async function getAll(): Promise<IUser[]> {
-    const db = await orm.openDb();
-    return db.users;
+  const db = await orm.openDb();
+  const users: IUser[] = db.users;
+  return users;
 }
-
 
 /**
  * Add one user.
- * 
- * @param user 
- * @returns 
+ *
+ * @param user
+ * @returns
  */
 async function add(user: IUser): Promise<void> {
-    const db = await orm.openDb();
-    user.id = getRandomInt();
-    db.users.push(user);
-    return orm.saveDb(db);
+  const db = await orm.openDb();
+  const users: IUser[] = db.users;
+  user.id = getRandomInt();
+  users.push(user);
+  db.users = users;
+  return orm.saveDb(db);
 }
-
 
 /**
  * Update a user.
- * 
- * @param user 
- * @returns 
+ *
+ * @param user
+ * @returns
  */
 async function update(user: IUser): Promise<void> {
-    const db = await orm.openDb();
-    for (let i = 0; i < db.users.length; i++) {
-        if (db.users[i].id === user.id) {
-            db.users[i] = user;
-            return orm.saveDb(db);
-        }
+  const db = await orm.openDb();
+  for (let i = 0; i < db.users.length; i++) {
+    if (db.users[i].id === user.id) {
+      db.users[i] = user;
+      return orm.saveDb(db);
     }
+  }
 }
-
 
 /**
  * Delete one user.
- * 
- * @param id 
- * @returns 
+ *
+ * @param id
+ * @returns
  */
 async function deleteOne(id: number): Promise<void> {
-    const db = await orm.openDb();
-    for (let i = 0; i < db.users.length; i++) {
-        if (db.users[i].id === id) {
-            db.users.splice(i, 1);
-            return orm.saveDb(db);
-        }
+  const db = await orm.openDb();
+  const users: IUser[] = db.users;
+  for (let i = 0; i < db.users.length; i++) {
+    if (db.users[i].id === id) {
+      users.splice(i, 1);
+      db.users = users;
+      return orm.saveDb(db);
     }
+  }
 }
-
 
 // Export default
 export default {
-    getOne,
-    persists,
-    getAll,
-    add,
-    update,
-    delete: deleteOne,
+  getOne,
+  persists,
+  getAll,
+  add,
+  update,
+  delete: deleteOne,
 } as const;
--- src/routes/user-router.ts
+++ src/routes/user-router.ts
@@ -1,10 +1,9 @@
 import StatusCodes from 'http-status-codes';
-import { Request, Response, Router } from 'express';
+import { Request, RequestHandler, Response, Router } from 'express';
 
 import userService from '@services/user-service';
 import { ParamMissingError } from '@shared/errors';
-
-
+import { IUser } from '@models/user-model';
 
 // Constants
 const router = Router();
@@ -12,67 +11,61 @@ const { CREATED, OK } = StatusCodes;
 
 // Paths
 export const p = {
-    get: '/all',
-    add: '/add',
-    update: '/update',
-    delete: '/delete/:id',
+  get: '/all',
+  add: '/add',
+  update: '/update',
+  delete: '/delete/:id',
 } as const;
-
-
 
 /**
  * Get all users.
  */
-router.get(p.get, async (_: Request, res: Response) => {
-    const users = await userService.getAll();
-    return res.status(OK).json({users});
-});
-
+router.get(p.get, (async (_: Request, res: Response) => {
+  const users = await userService.getAll();
+  return res.status(OK).json({ users });
+}) as RequestHandler);
 
 /**
  * Add one user.
  */
-router.post(p.add, async (req: Request, res: Response) => {
-    const { user } = req.body;
-    // Check param
-    if (!user) {
-        throw new ParamMissingError();
-    }
-    // Fetch data
-    await userService.addOne(user);
-    return res.status(CREATED).end();
-});
-
+router.post(p.add, (async (req: Request, res: Response) => {
+  const { user }: { user: IUser } = req.body;
+  // Check param
+  if (!user) {
+    throw new ParamMissingError();
+  }
+  // Fetch data
+  await userService.addOne(user);
+  return res.status(CREATED).end();
+}) as RequestHandler);
 
 /**
  * Update one user.
  */
-router.put(p.update, async (req: Request, res: Response) => {
-    const { user } = req.body;
-    // Check param
-    if (!user) {
-        throw new ParamMissingError();
-    }
-    // Fetch data
-    await userService.updateOne(user);
-    return res.status(OK).end();
-});
-
+router.put(p.update, (async (req: Request, res: Response) => {
+  const { user }: { user: IUser } = req.body;
+  // Check param
+  if (!user) {
+    throw new ParamMissingError();
+  }
+  // Fetch data
+  await userService.updateOne(user);
+  return res.status(OK).end();
+}) as RequestHandler);
 
 /**
  * Delete one user.
  */
-router.delete(p.delete, async (req: Request, res: Response) => {
-    const { id } = req.params;
-    // Check param
-    if (!id) {
-        throw new ParamMissingError();
-    }
-    // Fetch data
-    await userService.delete(Number(id));
-    return res.status(OK).end();
-});
-
+router.delete(p.delete, (async (req: Request, res: Response) => {
+  const { id } = req.params;
+  // Check param
+  if (!id) {
+    throw new ParamMissingError();
+  }
+  // Fetch data
+  await userService.delete(Number(id));
+  return res.status(OK).end();
+}) as RequestHandler);
 
 // Export default
 export default router;
